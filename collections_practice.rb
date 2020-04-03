ecd69934f1683620ffdab5b0b0fe8b37b36d9520
Dina Kendle <dina.kendle@gmail.com>
2020-04-03 20:09:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,12 +52,12 @@ end
 
 def add_s(array)
   
-  array.each_with_index do |word, index|
-    if index != 1
-      word = word + "s"
-    else
-      word = word
-    end
+  array.each_with_index.collect do |word,index|
+        if index != 1 
+          word = word + "s"
+        else
+          word = word
+        end
 end
 
 end
